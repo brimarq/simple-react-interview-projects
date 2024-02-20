11ae5e7e3869e6e11ca01c47b6844ea9136a380f
Brian Marquis <rbmarquis.dev@gmail.com>
2024-02-19 20:44:34 -0600

    create star rating component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import "./App.css";
-import Accordion from "./components/accordion";
-import RandomColor from "./components/random-color";
+// import Accordion from "./components/accordion";
+// import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 function App() {
   return (
     <>
       {/* Accordion component */}
-      <Accordion />
+      {/* <Accordion /> */}
       {/* Random color component */}
-      <RandomColor />
+      {/* <RandomColor /> */}
+      {/* Star rating component */}
+      <StarRating numStars={10} />
     </>
   );
 }
---
+++ src/components/star-rating/index.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import PropTypes from "prop-types";
+import "./styles.css";
+
+export default function StarRating({ numStars = 5 }) {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  function handleClick(getCurrentIndex) {
+    // console.log(getCurrentIndex);
+    setRating(getCurrentIndex);
+  }
+
+  function handleMouseEnter(getCurrentIndex) {
+    // console.log(getCurrentIndex);
+    setHover(getCurrentIndex);
+  }
+
+  function handleMouseLeave() {
+    setHover(rating);
+  }
+
+  return (
+    <div className="star-rating">
+      {[...Array(numStars)].map((_, index) => {
+        index += 1;
+
+        return (
+          <FaStar
+            key={index}
+            className={index <= (hover || rating) ? "active" : "inactive"}
+            onClick={() => handleClick(index)}
+            onMouseMove={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave()}
+            size={40}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+StarRating.propTypes = {
+  numStars: PropTypes.number,
+};
