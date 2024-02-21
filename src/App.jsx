d59d953fb1ffc9ef0cbbc0637b4a7fc14484294f
Brian Marquis <rbmarquis.dev@gmail.com>
2024-02-21 14:15:06 -0600

    create image slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
 // import Accordion from "./components/accordion";
 // import RandomColor from "./components/random-color";
-import StarRating from "./components/star-rating";
+// import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
 
 function App() {
   return (
@@ -11,9 +12,17 @@ function App() {
       {/* Random color component */}
       {/* <RandomColor /> */}
       {/* Star rating component */}
-      <StarRating numStars={10} />
+      {/* <StarRating numStars={10} /> */}
+      {/* Image slider component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </>
   );
 }
 
 export default App;
+
+// https://picsum.photos/v2/list?page=1&limit=5
