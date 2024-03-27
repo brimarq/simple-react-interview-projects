41e762e90b4a0de95c1e5477cfc55db2f6676e83
Brian Marquis <rbmarquis.dev@gmail.com>
2024-03-27 13:38:04 -0500

    finish useFetch custom hook

--- src/components/use-fetch/test.jsx
+++ src/components/use-fetch/test.jsx
@@ -6,12 +6,18 @@ export default function UseFetchHookTest() {
     {}
   );
 
-  console.log(error, data, pending);
+  // console.log(error, data, pending);
 
   return (
     <div>
       <h1>Use Fetch Hook</h1>
       {pending ? <h3>Pending... please wait...</h3> : null}
+      {error ? <h3>{error}</h3> : null}
+      {data && data.products && data.products.length
+        ? data.products.map((productItem) => (
+            <p key={productItem.id}>{productItem.title}</p>
+          ))
+        : null}
     </div>
   );
 }
